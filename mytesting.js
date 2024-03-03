512cf2b517ca5f84e91f3e046f0b1342afa1dfab
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-03 12:42:05 -0600

    figured it out. just have to return true in the else

--- mytesting.js
+++ mytesting.js
@@ -6,16 +6,20 @@ function init() {
                 type: 'input',
                 message: "Enter text (up to 3 characters):",
                 name: 'characterinput',
-              //  validate: function(input){
-              //      return /^a-zA-Z]{3,}$/.test(input);
-              //      // if (input === "1") {
-              //      //     return "Don't put the number 1 bro";
-              //      // }
-              //  }
+               validate: function(input){
+
+                   // return /^a-zA-Z]{3,}$/.test(input);
+
+                   if (input === "1") {
+                       return "Don't put the number 1 bro";
+                   } else {
+                    return true;
+                   }
+               }
             }
         ])
-        .then((answers) => {
-            console.log("Done! " + answers)
+        .then(() => {
+            console.log("Done!")
             return;
         });
 }
