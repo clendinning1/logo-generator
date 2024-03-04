b4fda60e998b61b9902c328c0b6e12ba8782cb02
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-03 18:48:30 -0600

    for some reason if statement is always running the else?

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Shape Classes!
+function Shape(baseTextInput, baseTextColor, baseShapeColor) {
+    this.baseTextInput = baseTextInput;
+    this.baseTextColor = baseTextColor;
+    this.baseShapeColor = baseShapeColor;
+};
+
+function Circle(baseTextInput, baseTextColor, baseShapeColor) {
+    Shape.call(this, baseTextInput, baseTextColor, baseShapeColor);
+    this.circleString = "<circle cx=\"150\" cy=\"100\" r=\"90\" fill=\"" + baseShapeColor + "\" /> \n <text x = \"150\" y = \"125\" fill = \"" + baseTextColor + "\" text-anchor=\"middle\" class=\"cirsq\" >" + baseTextInput + "</text>";
+};
+
+
+
 // inquirer funct
 function init() {
     const inquirer = require('inquirer');
@@ -58,35 +72,30 @@ function init() {
             },
         ])
         .then((response) => {
+            // svg strings that are the same for each shape
             const svg1 = "<svg width=\"300\" height=\"200\"> \n <style> \n .cirsq { \n font: bold 75px sans-serif; \n } .tri { \n font: bold 70px sans-serif; \n } \n </style> \n "
             const svg2 = " \n </svg>"
 
-            function Shape(baseTextInput, baseTextColor, baseShapeColor) {
-                this.baseTextInput = baseTextInput;
-                this.baseTextColor = baseTextColor;
-                this.baseShapeColor = baseShapeColor;
-            };
+            if (response.shapetype == "triangle") {
+                 console.log("Building your triangle...");
+             } else if (response.shapetype == "square") {
+                console.log("Building your square...");
+             } else {
+                console.log("Building your circle...");
+                const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
 
-            function Circle(baseTextInput, baseTextColor, baseShapeColor) {
-                Shape.call(this, baseTextInput, baseTextColor, baseShapeColor);
-                this.circleString = "<circle cx=\"150\" cy=\"100\" r=\"90\" fill=\"" + baseShapeColor + "\" /> \n <text x = \"150\" y = \"125\" fill = \"" + baseTextColor + "\" text-anchor=\"middle\" class=\"cirsq\" >" + baseTextInput + "</text>";
-            };
+                const fullCirString = svg1 + newCircle.circleString + svg2;
 
-            console.log("Building your circle...");
+                function createCircle() {
+                    const fs = require("fs");
 
-            const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
+                    fs.appendFile('logo.svg', fullCirString, (err) =>
+                        err ? console.error(err) : console.log("Generated logo.svg")
+                    );
+                }
 
-            const fullCirString = svg1 + newCircle.circleString + svg2;
-
-            function createCircle() {
-                const fs = require("fs");
-
-                fs.appendFile('logo.svg', fullCirString, (err) =>
-                    err ? console.error(err) : console.log("Generated logo.svg")
-                );
+                createCircle();
             }
-
-            createCircle();
 
             return;
         });
