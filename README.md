722fb36c12d64f1973c325452ae92c6c82801ed5
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-03-03 20:04:25 -0600

    added all color names

--- README.md
+++ README.md
@@ -15,3 +15,4 @@
 - https://www.thepoorcoder.com/how-to-check-if-a-color-is-valid-in-javascript/
 - https://stackoverflow.com/questions/54955165/create-a-equilateral-triangle-using-svg-in-html-with-base-and-height-100px
 - https://stackoverflow.com/questions/237104/how-do-i-check-if-an-array-includes-a-value-in-javascript
+- https://gist.github.com/bobspace/2712980
--- index.js
+++ index.js
@@ -1,8 +1,185 @@
-const { Triangle } = require("./lib/shapes.js");
-const { Circle } = require("./lib/shapes.js");
-const { Square } = require("./lib/shapes.js");
+// shape classes!
+function Shape(baseTextInput, baseTextColor, baseShapeColor) {
+    // setting up values to be inherited by its children
+    this.baseTextInput = baseTextInput;
+    this.baseTextColor = baseTextColor;
+    this.baseShapeColor = baseShapeColor;
+};
 
-const colorNames = ["blue", "yellow"];
+function Triangle(baseTextInput, baseTextColor, baseShapeColor) {
+    // calling from Shape class parent
+    Shape.call(this, baseTextInput, baseTextColor, baseShapeColor);
+    // string for the shape specific input into the svg file.
+    this.triangleString = "<polygon points=\"40 10, 260 10, 150 185\" style=\"fill: " + baseShapeColor + "\" /> \n <text x = \"145\" y = \"90\" fill = \"" + baseTextColor + "\" text-anchor=\"middle\" class=\"tri\" > " + baseTextInput + "</text>";
+};
+
+function Circle(baseTextInput, baseTextColor, baseShapeColor) {
+    // calling from Shape class parent
+    Shape.call(this, baseTextInput, baseTextColor, baseShapeColor);
+    // string for the shape specific input into the svg file.
+    this.circleString = "<circle cx=\"150\" cy=\"100\" r=\"90\" fill=\"" + baseShapeColor + "\" /> \n <text x = \"150\" y = \"125\" fill = \"" + baseTextColor + "\" text-anchor=\"middle\" class=\"cirsq\" >" + baseTextInput + "</text>";
+};
+
+function Square(baseTextInput, baseTextColor, baseShapeColor) {
+    // calling from Shape class parent
+    Shape.call(this, baseTextInput, baseTextColor, baseShapeColor);
+    // string for the shape specific input into the svg file.
+    this.squareString = "<rect x=\"65\" y=\"15\" width=\"170\" height=\"170\" fill=\"" + baseShapeColor + "\" /> \n <text x=\"150\" y=\"125\" fill= \"" + baseTextColor + "\" text-anchor=\"middle\" class=\"cirsq\">" + baseTextInput + "</text>";
+};
+
+
+// const { Triangle } = require("./lib/shapes.js");
+// const { Circle } = require("./lib/shapes.js");
+// const { Square } = require("./lib/shapes.js");
+
+const colorNames = [AliceBlue,
+    AntiqueWhite,
+    Aqua,
+    Aquamarine,
+    Azure,
+    Beige,
+    Bisque,
+    Black,
+    BlanchedAlmond,
+    Blue,
+    BlueViolet,
+    Brown,
+    BurlyWood,
+    CadetBlue,
+    Chartreuse,
+    Chocolate,
+    Coral,
+    CornflowerBlue,
+    Cornsilk,
+    Crimson,
+    Cyan,
+    DarkBlue,
+    DarkCyan,
+    DarkGoldenRod,
+    DarkGray,
+    DarkGrey,
+    DarkGreen,
+    DarkKhaki,
+    DarkMagenta,
+    DarkOliveGreen,
+    DarkOrange,
+    DarkOrchid,
+    DarkRed,
+    DarkSalmon,
+    DarkSeaGreen,
+    DarkSlateBlue,
+    DarkSlateGray,
+    DarkSlateGrey,
+    DarkTurquoise,
+    DarkViolet,
+    DeepPink,
+    DeepSkyBlue,
+    DimGray,
+    DimGrey,
+    DodgerBlue,
+    FireBrick,
+    FloralWhite,
+    ForestGreen,
+    Fuchsia,
+    Gainsboro,
+    GhostWhite,
+    Gold,
+    GoldenRod,
+    Gray,
+    Grey,
+    Green,
+    GreenYellow,
+    HoneyDew,
+    HotPink,
+    IndianRed,
+    Indigo,
+    Ivory,
+    Khaki,
+    Lavender,
+    LavenderBlush,
+    LawnGreen,
+    LemonChiffon,
+    LightBlue,
+    LightCoral,
+    LightCyan,
+    LightGoldenRodYellow,
+    LightGray,
+    LightGrey,
+    LightGreen,
+    LightPink,
+    LightSalmon,
+    LightSeaGreen,
+    LightSkyBlue,
+    LightSlateGray,
+    LightSlateGrey,
+    LightSteelBlue,
+    LightYellow,
+    Lime,
+    LimeGreen,
+    Linen,
+    Magenta,
+    Maroon,
+    MediumAquaMarine,
+    MediumBlue,
+    MediumOrchid,
+    MediumPurple,
+    MediumSeaGreen,
+    MediumSlateBlue,
+    MediumSpringGreen,
+    MediumTurquoise,
+    MediumVioletRed,
+    MidnightBlue,
+    MintCream,
+    MistyRose,
+    Moccasin,
+    NavajoWhite,
+    Navy,
+    OldLace,
+    Olive,
+    OliveDrab,
+    Orange,
+    OrangeRed,
+    Orchid,
+    PaleGoldenRod,
+    PaleGreen,
+    PaleTurquoise,
+    PaleVioletRed,
+    PapayaWhip,
+    PeachPuff,
+    Peru,
+    Pink,
+    Plum,
+    PowderBlue,
+    Purple,
+    RebeccaPurple,
+    Red,
+    RosyBrown,
+    RoyalBlue,
+    SaddleBrown,
+    Salmon,
+    SandyBrown,
+    SeaGreen,
+    SeaShell,
+    Sienna,
+    Silver,
+    SkyBlue,
+    SlateBlue,
+    SlateGray,
+    SlateGrey,
+    Snow,
+    SpringGreen,
+    SteelBlue,
+    Tan,
+    Teal,
+    Thistle,
+    Tomato,
+    Turquoise,
+    Violet,
+    Wheat,
+    White,
+    WhiteSmoke,
+    Yellow,
+    YellowGreen];
 
 // inquirer funct
 function init() {
@@ -122,7 +299,7 @@ function init() {
             } else {
                 // if circle... (same as above)
                 console.log("Building your circle...");
-                
+
                 const newCircle = new Circle(response.inqTextInput, response.inqTextColor, response.inqShapeColor);
                 const fullCirString = svg1 + newCircle.circleString + svg2;
 
